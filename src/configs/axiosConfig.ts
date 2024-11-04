4cc883c59d648a3ea0a06ec55271ecc6e72e02de
Dani lukman <danilukman2206@gmail.com>
2024-11-04 22:54:30 +0700

    asd

--- src/configs/axiosConfig.ts
+++ src/configs/axiosConfig.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 const axiosConfig = axios.create({
   // baseURL: `http://localhost:3000/api`
 
-  baseURL: `https://express-spp-api.sppapp.com/api`
+  baseURL: `https://express-spp-api.sppapp.my.id/api`
 })
 export default axiosConfig
