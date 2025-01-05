60063aab1b8617d8f574ad3e96bddcd43f2901b0
Dani lukman <danilukman2206@gmail.com>
2025-01-05 11:29:25 +0700

    asd

--- src/store/apps/absensi/hariLibur/index.tsx
+++ src/store/apps/absensi/hariLibur/index.tsx
@@ -1,15 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { Dispatch } from 'redux'
 import axiosConfig from 'src/configs/axiosConfig'
 
 interface DataParams {
   school_id: number
   q: string
   status: string
-}
-interface Redux {
-  getState: any
-  dispatch: Dispatch<any>
 }
 
 // ** Fetch Users
@@ -27,28 +22,21 @@ export const fetchDataHariLibur = createAsyncThunk('appUsers/fetchDataHariLibur'
   return response.data
 })
 
-export const deleteHariLibur = createAsyncThunk(
-  'appUsers/deleteHariLibur',
-  async (id: number | string, { getState, dispatch }: Redux) => {
-    const storedToken = window.localStorage.getItem('token')
-    const dataAll = {
-      data: id
-    }
-    const customConfig = {
-      headers: {
-        Accept: 'application/json',
-        Authorization: 'Bearer ' + storedToken
-      }
-    }
-    const response = await axiosConfig.post('/delete-holiday', dataAll, customConfig)
-    const { school_id, status, q } = getState().HariLibur
-
-    // Memanggil fetchDataHariLibur untuk memperbarui data setelah penghapusan
-    dispatch(fetchDataHariLibur({ school_id, status, q }))
-
-    return response.data
+export const deleteHariLibur = createAsyncThunk('appUsers/deleteHariLibur', async (id: number | string) => {
+  const storedToken = window.localStorage.getItem('token')
+  const dataAll = {
+    data: id
   }
-)
+  const customConfig = {
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer ' + storedToken
+    }
+  }
+  const response = await axiosConfig.post('/delete-holiday', dataAll, customConfig)
+
+  return response.data
+})
 export const appUsersSlice = createSlice({
   name: 'appUsers',
   initialState: {
