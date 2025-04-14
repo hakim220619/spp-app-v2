3d1078148fb9966dfdc0d6a91ae1c0574369ffde
Dani Lukman Hakin <itrnd@Hakim-MacBook-Pro.local>
2025-04-14 21:49:47 +0700

    asd

--- src/pages/ms/mataPelajaranGuru/progresSiswa/[uid].tsx
+++ src/pages/ms/mataPelajaranGuru/progresSiswa/[uid].tsx
@@ -11,7 +11,11 @@ import {
     DialogActions,
     DialogContent,
     DialogContentText,
-    DialogTitle
+    DialogTitle,
+    TextField,
+    MenuItem,
+    FormControl,
+    InputLabel
 } from '@mui/material'
 import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid'
 import Icon from 'src/@core/components/icon'
@@ -23,6 +27,11 @@ import { UsersType } from 'src/types/apps/userTypes'
 import TableHeader from 'src/pages/ms/kelas/TableHeader'
 import { useRouter } from 'next/router'
 import toast from 'react-hot-toast'
+import { useForm, Controller } from 'react-hook-form'
+import CustomTextField from 'src/@core/components/mui/text-field'
+import axiosConfig from 'src/configs/axiosConfig'
+import CustomAutocomplete from 'src/@core/components/mui/autocomplete'
+import { subject } from '@casl/ability'
 
 interface CellType {
     row: UsersType
@@ -130,12 +139,30 @@ const columns: GridColDef[] = [
 const UserList = () => {
     const data = localStorage.getItem('userData') as string
     const getDataLocal = JSON.parse(data)
+    const storedToken = window.localStorage.getItem('token')
+
     const [school_id] = useState<number>(getDataLocal.school_id)
     const [value, setValue] = useState<string>('')
     const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 })
     const [loading, setLoading] = useState<boolean>(true)
     const [status] = useState<any>('')
-    const [modalOpen, setModalOpen] = useState<boolean>(false) // State for controlling the modal visibility
+    const [modalOpen, setModalOpen] = useState<boolean>(false)
+
+    const [formData, setFormData] = useState({
+        description: '',
+        status: 'ON'
+    })
+    const { control, handleSubmit, formState: { errors }, reset } = useForm({
+        defaultValues: formData,
+    });
+    const [users, setUsers] = useState<any[]>([])
+    const [subjects, setSubjects] = useState<any[]>([]);
+    const router = useRouter()
+    const { uid } = router.query
+    const [subjectss_id] = useState<any>(router.query)
+
+    const [user_ids, setUserIds] = useState<string[]>([]);
+
     const dispatch = useDispatch<AppDispatch>()
     const store = useSelector((state: RootState) => state.ProgresSiswa)
 
@@ -149,11 +176,96 @@ const UserList = () => {
     const handleFilter = useCallback((val: string) => {
         setValue(val)
     }, [])
+
     const handleOpenModal = () => {
         setModalOpen(true)
     }
 
     const handleCloseModal = () => setModalOpen(false)
+
+    useEffect(() => {
+        const fetchSubjects = async () => {
+            if (!uid) return; // Make sure uid is ready before calling fetchSubjects
+            try {
+                const response = await axiosConfig.get(`/list-subjects/?schoolId=${school_id}`, {
+                    headers: {
+                        Accept: 'application/json',
+                        Authorization: `Bearer ${storedToken}`,
+                    },
+                });
+
+                const filteredSubjects = response.data.filter((subject: any) =>
+                    subject.id == uid
+                );
+
+                if (filteredSubjects.length > 0) {
+                    setSubjects(filteredSubjects[0].class_id); // Set the class_id from the fetched subject
+                }
+            } catch (error) {
+                console.error('Error fetching subjects:', error);
+            }
+        };
+
+        if (uid) {
+            fetchSubjects(); // Call fetchSubjects only when uid is available
+        }
+    }, [school_id, storedToken, uid]); // Add uid as a dependency
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            if (!subjects) return; // Make sure subjects state has been updated before calling fetchUsers
+            try {
+                const response = await axiosConfig.get(`/list-siswa/?schoolId=${school_id}`, {
+                    headers: {
+                        Accept: 'application/json',
+                        Authorization: `Bearer ${storedToken}`
+                    }
+                });
+
+                const filterUsersByClass = response.data.filter((data: any) => data.class_id == subjects);
+                console.log(subjects);
+
+                setUsers(filterUsersByClass);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+
+        if (subjects) {
+            fetchUsers();
+        }
+    }, [school_id, storedToken, subjects]);
+
+    console.log(subjectss_id);
+
+    // Handle form submission
+    const handleFormSubmit = (data: any) => {
+        const formData = new FormData()
+        formData.append('user_id', user_ids.join(','));
+        formData.append('subject_id', subjectss_id.uid);
+        formData.append('description', data.description)
+        formData.append('status', data.status)
+
+        const storedToken = window.localStorage.getItem('token')
+        axiosConfig
+            .post('/create-progresSiswa', formData, {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${storedToken}`
+                }
+            })
+            .then(response => {
+                console.log(response)
+                toast.success('Successfully Added Class!')
+                // router.push('/ms/kelas')
+            })
+            .catch(() => {
+                toast.error('Failed to add class')
+            })
+        handleCloseModal() // Close the modal after submission
+    }
+
 
     return (
         <Grid container spacing={6.5}>
@@ -192,22 +304,93 @@ const UserList = () => {
 
             {/* Modal for Filter Confirmation */}
             <Dialog open={modalOpen} onClose={handleCloseModal}>
-                <DialogTitle>Filter Applied</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        The filter has been applied. Do you want to proceed with this filter?
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseModal} color='primary'>
-                        Cancel
-                    </Button>
-                    <Button onClick={handleCloseModal} color='primary'>
-                        Confirm
-                    </Button>
-                </DialogActions>
+                <form onSubmit={handleSubmit(handleFormSubmit)}>
+
+                    <DialogTitle>Tambah Progres Siswa</DialogTitle>
+                    <DialogContent>
+                        <Grid container spacing={3}>
+                            {/* User ID Selection using CustomTextField */}
+                            <Grid item xs={12} sm={12} md={12}>
+                                <CustomAutocomplete
+                                    multiple  // Enable multiple selection
+                                    fullWidth
+                                    value={users.filter((data: any) => user_ids.includes(data.id)) || []}  // Handle multiple selected users (user_ids is an array)
+                                    options={users}
+                                    onChange={(event, newValue) => {
+                                        // Safely update the user_ids with the selected users' ids
+                                        setUserIds(newValue.map((user: any) => user.id));  // newValue is an array, so we update with an array of ids
+                                    }}
+                                    id="autocomplete-menu"
+                                    getOptionLabel={(option) => option.full_name || ''}
+                                    renderInput={(params) => <CustomTextField {...params} label="Users" variant="outlined" />}
+                                    renderOption={(props, option) => (
+                                        <li {...props} key={option.id}>
+                                            {option.full_name}
+                                        </li>
+                                    )}
+                                />
+                            </Grid>
+
+
+                            {/* Subject ID Selection using CustomTextField */}
+
+                            <Grid item xs={12} sm={12} md={12}>
+                                <Controller
+                                    name="description"
+                                    control={control}
+                                    defaultValue={formData.description}
+                                    rules={{ required: "Description is required" }}
+
+                                    render={({ field }) => (
+                                        <CustomTextField
+                                            {...field}
+                                            label="Description"
+                                            fullWidth
+                                            error={Boolean(errors.description)}
+
+                                        />
+                                    )}
+                                />
+                            </Grid>
+
+                            {/* Status Selection with Controller and CustomTextField */}
+                            <Grid item xs={12} sm={12} md={12}>
+                                <Controller
+                                    name="status"
+                                    control={control}
+                                    defaultValue={formData.status}
+                                    rules={{ required: "Status is required" }}
+                                    render={({ field }) => (
+                                        <CustomTextField
+                                            {...field}
+                                            label="Status"
+                                            fullWidth
+                                            select
+                                            error={Boolean(errors.status)}
+
+                                        >
+                                            <MenuItem value="ON">ON</MenuItem>
+                                            <MenuItem value="OFF">OFF</MenuItem>
+                                        </CustomTextField>
+                                    )}
+                                />
+                            </Grid>
+                            {/* Description TextField */}
+
+                        </Grid>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseModal} color='primary'>
+                            Cancel
+                        </Button>
+                        <Button type='submit' variant='contained'>
+                            Simpan
+                        </Button>
+                    </DialogActions>
+                </form>
+
             </Dialog>
-        </Grid>
+        </Grid >
     )
 }
 
