1787f14320da9e2896eea7ba94a0ff4282c76065
Dani Lukman Hakin <itrnd@Hakim-MacBook-Pro.local>
2025-05-31 03:49:43 +0700

    asd

--- src/configs/axiosConfig.ts
+++ src/configs/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosConfig = axios.create({
-  baseURL: `http://192.168.88.108:3088/api`
-  // baseURL: `https://express-spp-api.sppapp.my.id/api`
+  // baseURL: `http://192.168.88.108:3088/api`
+  baseURL: `https://express-spp-api.sppapp.my.id/api`
 })
 export default axiosConfig
